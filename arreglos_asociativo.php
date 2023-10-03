306afd56e87e4058884d813176bb3c95a8c9f1cd
Bruno Pereira <66494310+brunod3vs@users.noreply.github.com>
2023-10-03 20:23:36 -0300

    Add files via upload

---
+++ arreglos_asociativo.php
@@ -0,0 +1,9 @@
+<?php
+
+#quase como chaves, sao associativos
+$alex = array('telefono' => '123456789', 'edad' => 25, 'apellido' => 'Braga', 'pais' => 'Mexico');
+
+echo 'El telefono de Alex es: ' . $alex['telefono'] . '<br \>';
+echo 'la edad de Alex es: ' . $alex['edad'] . '<br \>';
+
+?>
---
+++ arreglos_multidimensionales.php
@@ -0,0 +1,11 @@
+<?php
+
+$amigos = array(
+    array('Alejandro', 20),
+    array('Cesar', 21),
+    array('Manuel', 18)
+);
+
+echo $amigos[1][0];
+
+?>
